9a8db678279e9474b3b68ba8507331e1c7c0717f
Danilo <danilo.koljensic.fit@gmail.com>
2024-01-23 18:59:58 +0100

    Added getRandomCard and optimize code for better function of the game

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
-let firstCard = 4
-let secondCard = 11
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let message = ""
 let hasBlackJack = false
@@ -9,34 +10,51 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("card-el")
 
-function startGame() {
-    cardsEl.textContent += firstCard + " " + secondCard
-    sumEl.textContent += sum
 
+function getRandomCard() {
+
+    return Math.floor(Math.random() * 10) + 1
+
+}
+
+function startGame() {
+    renderGame()
+    
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    sumEl.textContent = "Sum: " + sum
+    for(let i = 0; i < cards.length; i++) {
+
+        cardsEl.textContent +=  " " + cards[i] 
+             
+    }
+     
     if(sum <= 20) {
-        message += "Do you want to draw a new card?"
-        
-        
+        message = "Do you want to draw a new card?"
         
     }else if(sum === 21) {
-        message += "Wohoo! You've got Blackjack!"
+        message = "Wohoo! You've got Blackjack!"
         hasBlackJack = true
         
     }else {
-        message += message = "You're out of the game!"  
+        message = message = "You're out of the game!"  
         isAlive = false
     }
 
 
     messageEl.textContent = message
     
-    
+       
 }
 
 
 function newCard() {
 
-    let newCard = 2
+    let newCard = getRandomCard()
     sum += newCard
-    startGame()
+    cards.push(newCard)
+    renderGame()
+
 }
