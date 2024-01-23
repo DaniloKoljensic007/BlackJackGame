44d500e653dce02e891eace8e705a78cee2b4602
Danilo <danilo.koljensic.fit@gmail.com>
2024-01-23 13:48:25 +0100

    Improved logic behind setting the message and sum and adding newCard button to add newCard

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-let firstCard = 10
+let firstCard = 4
 let secondCard = 11
 let sum = firstCard + secondCard
 let message = ""
@@ -7,29 +7,36 @@ let hasBlackJack = false
 let isAlive = true
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
-
+let cardsEl = document.getElementById("card-el")
 
 function startGame() {
- 
-    
+    cardsEl.textContent += firstCard + " " + secondCard
+    sumEl.textContent += sum
+
     if(sum <= 20) {
         message += "Do you want to draw a new card?"
-        sumEl.textContent += sum
+        
+        
         
     }else if(sum === 21) {
         message += "Wohoo! You've got Blackjack!"
-        sumEl.textContent +=  sum
         hasBlackJack = true
         
     }else {
-        message += message = "You're out of the game!"
-        sumEl.textContent += sum      
+        message += message = "You're out of the game!"  
         isAlive = false
     }
 
 
     messageEl.textContent = message
-    messageEl = ""
-    sumEl = ""
     
+    
+}
+
+
+function newCard() {
+
+    let newCard = 2
+    sum += newCard
+    startGame()
 }
