e26b6a259332c2067ee2b2ec21d300c8e2504c90
Danilo <danilo.koljensic.fit@gmail.com>
2024-01-23 12:42:30 +0100

    Adding logic to display message based on number of cards and displaying sum

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 
-let firstCard = 9
+let firstCard = 10
 let secondCard = 11
 let sum = firstCard + secondCard
 let message = ""
 let hasBlackJack = false
 let isAlive = true
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+
 
 function startGame() {
+ 
+    
     if(sum <= 20) {
-        message += "Do you want to draw a new card? 🙂"
+        message += "Do you want to draw a new card?"
+        sumEl.textContent += sum
+        
     }else if(sum === 21) {
-        message += "Wohoo! You've got Blackjack! 🥳"
+        message += "Wohoo! You've got Blackjack!"
+        sumEl.textContent +=  sum
         hasBlackJack = true
+        
     }else {
-        message += message = "You're out of the game! 😭"
+        message += message = "You're out of the game!"
+        sumEl.textContent += sum      
         isAlive = false
     }
-    
-    console.log(message)
 
+
+    messageEl.textContent = message
+    messageEl = ""
+    sumEl = ""
+    
 }
